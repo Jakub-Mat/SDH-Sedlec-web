21e92b77587818908f552a7dbaa457a21d42ce00
Jakub-Mat <chlebak2801@gmail.com>
2025-10-01 23:23:48 +0200

    Changes: Adding clickable icons Making better responsibility - because icons make it messy Reaparing bad language in navigation menu And I also added animation (but it took much longer than I expected)

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,9 @@ import facebook from "./assets/svg/facebook-square-icon.svg"
 
 function App() {
   const [menuActive, setMenuActive] = useState(false);
-  
+  const [firetruckClicked, setFiretruckClicked] = useState(false);
+  const [redPplClicked, setRedPplClicked] = useState(false);
+  const [bluePplClicked, setBluePplClicked] = useState(false);
 
   const toggleMenu = () => {
     setMenuActive(!menuActive);
@@ -30,9 +32,10 @@ function App() {
           ☰
         </button>
         <nav className={`nav-menu ${menuActive ? 'active' : ''}`}>
-          <a href="#home" className="nav-link" id="activeCard">Home</a>
-          <a href="#about" className="nav-link">About</a>
-          <a href="#contact" className="nav-link">Contact</a>
+          <a href="#home" className="nav-link" id="activeCard">Domů</a>
+          <a href="#about" className="nav-link">Materiály</a>
+          <a href="#contact" className="nav-link">Členové</a>
+          <a href="#contact" className="nav-link">Kronika</a>
           <a href="#login" className="nav-link login-link">Login</a>
         </nav>
       </header>
@@ -47,18 +50,70 @@ function App() {
           </p>
           <div className="sdh-stats">
             <div className="sdh-stat">
-              <img src={redFiretruck} alt="Hasičské auto" className="sdh-icon" />
-              <div className="sdh-stat-label">4 Hasičské Vozy</div>
+              <img
+              // red firetruck icon
+              src={redFiretruck}
+              alt="Hasičské auto" 
+              className="sdh-icon" 
+              id="firetruck" 
+              onClick={() => setFiretruckClicked(!firetruckClicked) }
+              />
+              <div className="sdh-stat-label" >4 Hasičské Vozy</div>
+                <div className={`additional-dropdown ${firetruckClicked ? 'open' : 'closed'}`}>
+                  <p>Naše hasičské vozy jsou vybaveny nejmodernější technikou a připraveny k okamžitému zásahu 24/7.</p>
+                    <a href="" target="_self" rel="noopener noreferrer">
+                    <p>Konkrétní typy vozidel</p>
+                  </a>
+                </div>
             </div>
-            <div className="sdh-divider" />
+
+            <div 
+              // divider mezi ikonami zmizí pokud jsou rozbaleny 2 nebo více dropdowny 
+              className={"sdh-divider"}
+              id={(firetruckClicked && redPplClicked || bluePplClicked && firetruckClicked || redPplClicked && bluePplClicked ? "extra" : "")}
+ 
+            />
+
             <div className="sdh-stat">
-              <img src={redFirefighter} alt="Člen jednotky" className="sdh-icon" />
+              <img
+                // red people icon
+                src={redFirefighter} 
+                alt="Člen jednotky" 
+                className="sdh-icon" 
+                id="redPpl"
+                onClick={() => setRedPplClicked(!redPplClicked)}
+              />
               <div className="sdh-stat-label">15 Členů Jednotky</div>
+                <div className={`additional-dropdown ${redPplClicked ? 'open' : 'closed'}`}>
+                  <p>Náš tým tvoří 15 odvážných a vysoce kvalifikovaných členů, kteří jsou připraveni zasáhnout v každé situaci.</p>
+                    <a href="" target="_self" rel="noopener noreferrer">
+                    <p>Seznam členů</p>
+                  </a>
+                </div>
+              
             </div>
-            <div className="sdh-divider" />
+
+            <div
+              className={"sdh-divider"}
+              id={(firetruckClicked && redPplClicked || bluePplClicked && firetruckClicked || redPplClicked && bluePplClicked ? "extra" : "")}
+            />
+
             <div className="sdh-stat">
-              <img src={blueDiversity} alt="Šťastné děti" className="sdh-icon" />
+              <img
+                // blue people icon
+                src={blueDiversity} 
+                alt="Šťastné děti" 
+                className="sdh-icon" 
+                id="bluePpl"
+                onClick={() => setBluePplClicked(!bluePplClicked)}
+              />
               <div className="sdh-stat-label">20+ Šťastných Dětí</div>
+                <div className={`additional-dropdown ${bluePplClicked ? 'open' : 'closed'}`}>
+                  <p>Naše dětské oddíly jsou plné radosti, přátelství a dobrodružství, kde se děti učí hodnotám jako je týmová práce a odvaha.</p>
+                    <a href="" target="_self" rel="noopener noreferrer">
+                    <p>Více o dětských oddílech</p>
+                  </a>
+                </div>
             </div>
           </div>
         </section>
